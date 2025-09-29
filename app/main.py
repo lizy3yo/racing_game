dfcc99b12dd976c51c4e2af386ef7b7545fda162
unknown <dejesuskharl32@gmail.com>
2025-09-29 22:45:49 +0800

    Crash and Finish

--- app/main.py
+++ app/main.py
@@ -4,12 +4,16 @@ import math
 from utils import scale_image, blit_rotate_center
 
 GRASS = scale_image(pygame.image.load('imgs/grass.jpg'), 2.5) 
-TRACK = scale_image(pygame.image.load('imgs/circle.jpg'), 0.9)
+TRACK = scale_image(pygame.image.load('imgs/track.png'), 0.9)
 
 TRACK_BORDER = scale_image(pygame.image.load('imgs/track-border.png'), 0.9)
-FINISH = pygame.image.load('imgs/finish.png')
+TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
 
-FERARRI = scale_image(pygame.image.load('imgs/ferrari.png'), 1)
+FINISH = pygame.image.load('imgs/finish.png')
+FINISH_MASK = pygame.mask.from_surface(FINISH)
+FINISH_POSITION = (130, 250)
+
+FERARRI = scale_image(pygame.image.load('imgs/ferrari.png'), 0.55)
 BLUE_CAR = scale_image(pygame.image.load('imgs/white-car.png'), 0.55)
 
 # Create window using track size, then set caption via pygame.display
@@ -42,6 +46,10 @@ class AbstractCar:
     def move_forward(self):
         self.vel = min(self.vel + self.acceleration, self.max_vel)
         self.move()
+
+    def move_backward(self):
+        self.vel = max(self.vel - self.acceleration, -self.max_vel/2)
+        self.move()
     
     def move(self):
         radias = math.radians(self.angle)
@@ -50,14 +58,30 @@ class AbstractCar:
        
         self.x -= horizontal
         self.y -= vertical
+    
+    def collide(self, mask, x=0, y=0):
+        car_mask = pygame.mask.from_surface(self.img)
+        offset = (int(self.x - x), int(self.y - y))
+        poi = mask.overlap(car_mask, offset)
+        return poi
+    
+    def reset(self):
+        self.x, self.y = self.START_POS
+        self.angle = 0
+        self.vel = 0
+
+class PlayerCar(AbstractCar):
+    IMG = FERARRI
+    START_POS = (180, 200)
 
     def reduce_speed(self):
         self.vel = max(self.vel - self.acceleration / 2, 0)
         self.move()
 
-class PlayerCar(AbstractCar):
-    IMG = FERARRI
-    START_POS = (180, 200)
+    def bounce(self):
+        self.vel = -self.vel
+        self.move() 
+
 
 
 def draw (win, images, player_car):
@@ -67,9 +91,27 @@ def draw (win, images, player_car):
     player_car.draw(win)
     pygame.display.update()
 
+def move_player(player_car):
+    keys = pygame.key.get_pressed()
+    moved = False
+
+    if keys[pygame.K_a]:
+        player_car.rotate(left=True)
+    if keys[pygame.K_d]:
+        player_car.rotate(right=True)
+    if keys[pygame.K_w]:
+        moved = True
+        player_car.move_forward()
+    if keys[pygame.K_s]:
+        moved = True
+        player_car.move_backward()
+    
+    if not moved:
+        player_car.reduce_speed()
+
 run = True
 clock = pygame.time.Clock()
-images = [(GRASS, (0,0)), (TRACK, (0,0))]
+images = [(GRASS, (0,0)), (TRACK, (0,0)), (FINISH, FINISH_POSITION), (TRACK_BORDER, (0,0))]
 player_car = PlayerCar(4, 4) 
 
 while run:
@@ -84,19 +126,23 @@ while run:
             run = False
             break
 
-    keys = pygame.key.get_pressed()
-    moved = False
+    move_player(player_car)
 
-    if keys[pygame.K_a]:
-        player_car.rotate(left=True)
-    if keys[pygame.K_d]:
-        player_car.rotate(right=True)
-    if keys[pygame.K_w]:
-        moved = True
-        player_car.move_forward()
 
-    if not moved:
-        player_car.reduce_speed()
+    if player_car.collide(TRACK_BORDER_MASK) != None:
+        player_car.bounce()
+
+
+    finish_poi_collide = player_car.collide(FINISH_MASK, *FINISH_POSITION)
+    if finish_poi_collide != None:
+        if finish_poi_collide[1] == 0:
+            player_car.bounce()
+        else:
+            player_car.reset()
+            print("You Win!")
+
+        
+
 
 pygame.quit()
  
